7d6a9d23e07a9370364d29b7e1d24007fb1502a1
Ty Jenkins <cientifik85@gmail.com>
2020-04-13 17:13:09 -0400

    fix: apikey file fixed for personal no and logging key

--- src/ApiKey.ts
+++ src/ApiKey.ts
@@ -3,8 +3,8 @@
  */
 function getKey(): string {
   const keysArray = process.env.KEYS_ARRAY;
-  const randomKey = keysArray[Math.floor(Math.random() * keysArray.length)];
-  return randomKey;
+  // const randomKey = keysArray[Math.floor(Math.random() * keysArray.length)];
+  return keysArray;
 }
 
 export default getKey;
